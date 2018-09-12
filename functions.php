653c32ada164a50cfe84af778cc4e140e042a52f
Rodica-Elena Andronache <rodica.andronache@vertistudio.com>
2018-09-12 18:04:17 +0300

    Deploy mechanism

--- functions.php
+++ functions.php
@@ -8,6 +8,17 @@
 
 define( 'JINSY_MAGAZINE_VERSION', '1.0.0' );
 
+$vendor_file = trailingslashit( get_template_directory() ) . 'vendor/autoload.php';
+if ( is_readable( $vendor_file ) ) {
+    require_once $vendor_file;
+}
+add_filter(
+    'themeisle_sdk_products', function ( $products ) {
+        $products[] = get_template_directory() . '/style.css';
+        return $products;
+    }
+);
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
