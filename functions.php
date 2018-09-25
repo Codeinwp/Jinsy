b17e258815a30f0b95be772d076c5a5c94783152
coti <stefan@themeisle.com>
2018-09-25 20:29:36 +0300

    use the third header layout to disable the header the title still appears

--- functions.php
+++ functions.php
@@ -47,6 +47,10 @@ if ( file_exists( get_stylesheet_directory() . '/inc/inline-style.php' ) ) {
 	require_once get_stylesheet_directory() . '/inc/inline-style.php';
 }
 
+if ( file_exists( get_stylesheet_directory() . '/inc/disable-header.php' ) ) {
+	require_once get_stylesheet_directory() . '/inc/disable-header.php';
+}
+
 /**
  * Default accent color
  */
@@ -263,3 +267,4 @@ function jinsy_magazine_theme_setup() {
 }
 
 add_action( 'after_setup_theme', 'jinsy_magazine_theme_setup' );
+
