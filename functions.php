7012896ac0c8479b8b1b6f9f8ae51c4b6cd2d7df
coti <stefan@themeisle.com>
2018-12-14 16:12:10 +0200

    fix: Change default font in customizer preview too
    Default font was changed on the front end only.

--- functions.php
+++ functions.php
@@ -86,6 +86,25 @@ add_filter( 'hestia_headings_default', 'jinsy_magazine_font_default' );
 add_filter( 'hestia_body_font_default', 'jinsy_magazine_font_default' );
 
 /**
+ * Default font in customizer preview
+ *
+ * @param object $wp_customize the customizer manager.
+ */
+function jinsy_magazine_customizer_font_default( $wp_customize ) {
+
+	$jinsy_magazine_headings_font = $wp_customize->get_setting( 'hestia_headings_font' );
+	if ( ! empty( $jinsy_magazine_headings_font ) ) {
+		$jinsy_magazine_headings_font->default = jinsy_magazine_font_default();
+	}
+
+	$jinsy_magazine_body_font = $wp_customize->get_setting( 'hestia_body_font' );
+	if ( ! empty( $jinsy_magazine_body_font ) ) {
+		$jinsy_magazine_body_font->default = jinsy_magazine_font_default();
+	}
+}
+add_action( 'customize_register', 'jinsy_magazine_customizer_font_default', 99 );
+
+/**
  * Default boxed-layout
  */
 function jinsy_magazine_boxed_layout_default() {
